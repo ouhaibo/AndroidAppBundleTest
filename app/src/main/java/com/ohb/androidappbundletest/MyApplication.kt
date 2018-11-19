588e59d4284b293fb8c3d5228b68be41e050c7cf
ouhaibo <ouhaibo@camera360.com>
2018-11-19 16:53:48 +0800

    add

---
+++ app/src/main/java/com/ohb/androidappbundletest/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.ohb.androidappbundletest
+
+import android.app.Application
+import android.content.Context
+import com.google.android.play.core.splitcompat.SplitCompat
+
+class MyApplication : Application() {
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        SplitCompat.install(this)
+    }
+}
